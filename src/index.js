915950508e4f486a008f9dd548b9c26874418a3e
Guadas1103 <lupdom19@gmail.com>
2026-02-12 14:07:47 -0600

    Proyecto adaptado para GitHub Pages
    modificaciones en pacjage.jason y en index para el correcto funcionamiento

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext"; 
 import "./styles/global.css"; 
 import "./styles/Navbar.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <AuthProvider> {/* Aquí envuelves la app con AuthProvider */}
+  <HashRouter>
+    <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
